49486aa5c965046fb250294ac4046b8d8af4075f
Peiiyong <jemins2001@gmail.com>
2025-04-12 17:13:11 +0800

    update splash screen

---
+++ lib/view/splashScreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:s/view/homeScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/image1.png'),
+
+            Text('Random User Info Viewer'),
+
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
